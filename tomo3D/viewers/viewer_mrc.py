0df471e12bf4018666a462c2d3a55e01e9128f95
DavidHerreros <david.herreroscalero@gmail.com>
2021-10-08 13:08:41 +0200

    Fix colormap name

--- tomo3D/viewers/viewer_mrc.py
+++ tomo3D/viewers/viewer_mrc.py
@@ -97,7 +97,7 @@ class MrcPlot(object):
             self.points /= 2 ** self.binning  # Binning Scaling
             self.pv_points = pv.PolyData(self.points)
             scalar_colors = np.zeros([self.points.shape[0], 4])
-            cmap = matplotlib.cm.get_cmap('viridis')
+            cmap = matplotlib.cm.get_cmap('gist_rainbow_r')
             unique_ids = np.unique(self.group_ids)
             cmap_ids = np.linspace(0, 1, len(unique_ids))
             for group_id, cmap_id in zip(unique_ids, cmap_ids):
